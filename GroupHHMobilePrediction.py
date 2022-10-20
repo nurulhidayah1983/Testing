e67f5340fe63c59b8d5332afcf49c2e08158cd75
nurulhidayah1983 <39686007+nurulhidayah1983@users.noreply.github.com>
2022-10-20 17:53:29 +0800

    Update GroupHHMobilePrediction.py

--- GroupHHMobilePrediction.py
+++ GroupHHMobilePrediction.py
@@ -45,12 +45,12 @@ st.pyplot(fig)
 
 st.write("Conclusion: The most correlated features are:price_range vs Ram has correlation coefficient of 0.92.")
 
-
-data_modelling = dcopy_new.drop(useless_col, axis = 1)
 useless_col = ['battery_power', 'blue', 'clock_speed', 'dual_sim', 'fc', 'four_g',
        'int_memory', 'm_dep', 'mobile_wt', 'n_cores', 'pc', 'px_height',
        'px_width', 'ram', 'sc_h', 'sc_w', 'talk_time', 'three_g',
        'touch_screen', 'wifi']
+data_modelling = dcopy_new.drop(useless_col, axis = 1)
+
 y = data_modelling['price_range']
 X1 = data_modelling.drop('price_range', axis = 1)
 X2 = pd.get_dummies(data_modelling)
