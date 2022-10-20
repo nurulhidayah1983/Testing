a87a0e975e9fde049ffe35ab48483ecdf025d18b
nurulhidayah1983 <39686007+nurulhidayah1983@users.noreply.github.com>
2022-10-20 22:44:39 +0800

    Update GroupHHMobilePrediction.py

--- GroupHHMobilePrediction.py
+++ GroupHHMobilePrediction.py
@@ -21,38 +21,15 @@ st.write('This is a pie chart for price range.')
 pie_chart = px.pie(data,"price_range")
 st.plotly_chart(pie_chart)
 
-#st.write('This is a outlier for px_height.')
-#arr = np.random.normal(1, 1, size=100)
-#fig, ax = plt.subplots()
-#ax.box([data.fc], bins=20)
-#st.pyplot(fig)
-
-#st.write('This is a outlier for px_height.')
-#fig,ax = plt.subplots()
-#ax.box(data['px_height'],bins=20)
-#st.pyplot(fig)
-
-st.write('To see correlation')
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import tree
 from sklearn.metrics import accuracy_score
 
 dcopy=data.copy()
-
 dcopy_new=dcopy
-
 dcopy_new[['clock_speed', 'm_dep','fc','px_height']] = dcopy[['clock_speed', 'm_dep','fc','px_height']].astype('int64') 
 
-#matrix = dcopy.corr()
-#f, ax = plt.subplots(figsize=(20, 15))
-#sns.heatmap(matrix, vmax=1, square=True, annot=True,cmap='Paired')
-
-#fig, ax = plt.subplots()
-#sns.heatmap(matrix, ax=ax)
-#st.pyplot(fig)
-
-#if st.checkbox("Show Correlation Plot")
 st.write("Show Correlation Plot")
 st.write("### Heatmap")
 fig, ax = plt.subplots(figsize=(30,15))
@@ -101,21 +78,20 @@ st.write('Accuracy of Logistic Regression (without PCA) on training set: {:.2f}'
 st.write('Accuracy of Logistic Regression (without PCA)  on testing set: {:.2f}'
      .format(logregwithoutpca.score(X_test, y_test)))
 st.write('\nConfusion matrix :\n',confusion_matrix(y_test, logregwithoutpca_result))
-#print('\n\nClassification report :\n\n', classification_report(y_test, logregwithoutpca_result))
-
-#print
+st.write('\n\nClassification report :\n\n', classification_report(y_test, logregwithoutpca_result))
 
 #ConfusionMatrix 
 
 st.write("Visualization Confusion Matrix")
+
 confusion_matrix =confusion_matrix(y_test, logregwithoutpca_result)
+
 sns.heatmap(confusion_matrix, annot=True, fmt="d", cmap='Set3')
 plt.title('Confusion Matrix for KNN')
 plt.xlabel('Predicted')
 plt.ylabel('True')
 st.write(classification_report(y_test, logregwithoutpca_result))
 st.pyplot()
-
 
 
 st.write("ConfusionMatrix In Percentage")
@@ -125,38 +101,29 @@ plt.title('Confusion Matrix for KNN In Percentage')
 plt.xlabel('Predicted Value')
 plt.ylabel('True')
 st.write(classification_report(y_test, logregwithoutpca_result))
-
 fig, ax = plt.subplots()
+#sns.heatmap(confusion_matrix, ax=ax)
+#st.write(classification_report(y_test, logregwithoutpca_result))
+#st.pyplot(fig)
 
-sns.heatmap(confusion_matrix, ax=ax)
-st.write(classification_report(y_test, logregwithoutpca_result))
-st.pyplot(fig)
-
-
-
-
+st.write("ConfusionMatrix Actual Value and Percentage")
 group_counts = ["{0:0.0f}".format(value) for value in
                 confusion_matrix.flatten()]
 group_percentages = ["{0:.1%}".format(value) for value in
                      confusion_matrix.flatten()/np.sum(confusion_matrix)]
-
 labels = [f"{v2}\n{v3}" for v2, v3 in
           zip(group_counts,group_percentages)]
 labels = np.asarray(labels).reshape(4,4)
 sns.heatmap(confusion_matrix, annot=labels, fmt='', cmap='Pastel1')
-
 plt.title('Confusion Matrix for KNN ')
-
 fig, ax = plt.subplots()
 sns.heatmap(confusion_matrix, ax=ax)
 st.pyplot(fig)
-
 
 
 
 plt.clf()
 plt.imshow(confusion_matrix, interpolation='nearest', cmap=plt.cm.Pastel2)
-
 classNames = ['Negative','Positive','Positive','Positive']
 plt.title('Mobile Phone Perdiction Confusion Matrix')
 plt.ylabel('True label')
@@ -170,14 +137,12 @@ for i in range(4):
         plt.text(j,i, str(s[i][j])+" = "+str(confusion_matrix[i][j]))
 plt.show()
 
+
 fig, ax = plt.subplots()
 sns.heatmap(confusion_matrix, ax=ax)
 st.pyplot(fig)
-
 st.write("Data Visualisation:")
-
 sns.jointplot(x='ram',y='price_range',data=dcopy_new,color='brown',kind='kde');   
-
 sns.pointplot(y="int_memory", x="price_range", data=dcopy_new)
 
   
