a56a805d91cc4a4efc65d004e0cca43ed6e1b3ea
nurulhidayah1983 <39686007+nurulhidayah1983@users.noreply.github.com>
2022-10-20 15:02:15 +0800

    Update GroupHHMobilePrediction.py

--- GroupHHMobilePrediction.py
+++ GroupHHMobilePrediction.py
@@ -3,17 +3,21 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.express as px
 
 st.header("Mobile Prediction project")
 
 data = pd.read_csv(r'train.csv',)
-st.write(data.head())
 
-st.write (data.isna().sum())
-st.write (data.columns)
+if st.checkbox('Show Dataframe'):
+  st.write(data)
+  
+  st.write('This is a column.')
+  st.write (data.columns)
 
+st.write('This is a pie chart for price range.')
 
-fig, ax = plt.subplots()
-ax.box(data['battery_power'], bins=20)
+pie_chart = px.pie(data,"price_range")
+st.plotly_chart(pie_chart)
 
-st.pyplot(fig)
+st.write('This is a outlier for px_height.')
