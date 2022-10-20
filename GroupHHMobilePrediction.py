6c5657ca8817e18e6dad300c9dadca97daf4305c
nurulhidayah1983 <39686007+nurulhidayah1983@users.noreply.github.com>
2022-10-20 23:37:36 +0800

    Update GroupHHMobilePrediction.py

--- GroupHHMobilePrediction.py
+++ GroupHHMobilePrediction.py
@@ -133,8 +133,8 @@ fig, ax = plt.subplots()
 sns.heatmap(confusion_matrix, ax=ax)
 st.pyplot(fig)
 st.write("Data Visualisation:")
-sns.jointplot(x='ram',y='price_range',data=dcopy_new,color='brown',kind='kde');   
-sns.pointplot(y="int_memory", x="price_range", data=dcopy_new)
+st.write(sns.jointplot(x='ram',y='price_range',data=dcopy_new,color='brown',kind='kde'));   
+st.write(sns.pointplot(y="int_memory", x="price_range", data=dcopy_new))
 
   
          
