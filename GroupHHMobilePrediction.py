e23f9c1a9b6300453a6be62a2ff6aedcb5f8a473
nurulhidayah1983 <39686007+nurulhidayah1983@users.noreply.github.com>
2022-10-26 15:17:32 +0800

    Update GroupHHMobilePrediction.py

--- GroupHHMobilePrediction.py
+++ GroupHHMobilePrediction.py
@@ -79,13 +79,11 @@ clf.fit(X, Y)
 prediction = clf.predict(dataframe)
 prediction_proba = clf.predict_proba(dataframe)
 
-st.subheader('Class labels and their corresponding index number')
-st.write(data.price_range)
+#st.subheader('Class labels and their corresponding index number')
+#st.write(data.price_range)
          
-st.subheader('Prediction')
+st.subheader('Phone Price Prediction')
 st.write(data.price_range)
-#st.write(prediction)
-#st.write(data.target_names[price_range])
 st.write(prediction)
 
 st.subheader('Prediction Probability')
